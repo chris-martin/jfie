6a2ceefd3f41589ebea22d0876a1f56fbac004b0
Christopher Martin <ch.martin@gmail.com>
2012-02-24 01:15:09 -0500

    asterisks

--- src/main/java/org/chris_martin/jfie/Jfie.java
+++ src/main/java/org/chris_martin/jfie/Jfie.java
@@ -4,40 +4,38 @@ import static org.chris_martin.jfie.FactoryLists.factoryList;
 import static org.chris_martin.jfie.JfieException.newJfieException;
 
 /**
-
-<p style="font-weight: bold;">Classes with multiple constructors</p>
-
-<p>JFIE does not fail when constructor choice is ambiguous; it just fucking
-injects something. Prefer higher-arity constructors over fewer args. Our job
-is to inject, so damn it we're going to inject as many things as possible.</p>
-
-<p>JFIE will never call a constructor with a {@code null} argument.
-If it can't find a constructor for which instances can be provided
-for all arguments, it will fail.</p>
-
-<p style="font-weight: bold;">Injecting the same type into a class repeatedly</p>
-
-<p>This is allowed. If you have multiple constructor args, public fields,
-and setter methods that all take the same type, then you're going to end up
-getting injected with a lot of instances.</p>
-
-<p style="font-weight: bold;">Ambiguous types</p>
-
-<p>If you give JFIE two classes and then ask it for a supertype of those classes,
-JFIE will tell you to {@link JfieException.BeMoreSpecific}. Look, I know this is
-supposed to be a really permissive tool and all, but there are limits. I'm not
-going to start guessing which classes you want.</p>
-
-<p style="font-weight: bold;">Generic types</p>
-
-<p>Fuck you, this tool doesn't support generic types. Java generics suck anyway.</p>
-
-<p style="font-weight: bold;">Circular dependencies</p>
-
-<p>Circular dependencies in fields and setter methods are absolutely okay.
-In constructors, they are absolutely <em>not</em> okay. JFIE will throw up
-if it notices even the possibility of any of that shit going on.</p>
-
+ * <p style="font-weight: bold;">Classes with multiple constructors</p>
+ *
+ * <p>JFIE does not fail when constructor choice is ambiguous; it just fucking
+ * injects something. Prefer higher-arity constructors over fewer args. Our job
+ * is to inject, so damn it we're going to inject as many things as possible.</p>
+ *
+ * <p>JFIE will never call a constructor with a {@code null} argument.
+ * If it can't find a constructor for which instances can be provided
+ * for all arguments, it will fail.</p>
+ *
+ * <p style="font-weight: bold;">Injecting the same type into a class repeatedly</p>
+ *
+ * <p>This is allowed. If you have multiple constructor args, public fields,
+ * and setter methods that all take the same type, then you're going to end up
+ * getting injected with a lot of instances.</p>
+ *
+ * <p style="font-weight: bold;">Ambiguous types</p>
+ *
+ * <p>If you give JFIE two classes and then ask it for a supertype of those classes,
+ * JFIE will tell you to {@link JfieException.BeMoreSpecific}. Look, I know this is
+ * supposed to be a really permissive tool and all, but there are limits. I'm not
+ * going to start guessing which classes you want.</p>
+ *
+ * <p style="font-weight: bold;">Generic types</p>
+ *
+ * <p>Fuck you, this tool doesn't support generic types. Java generics suck anyway.</p>
+ *
+ * <p style="font-weight: bold;">Circular dependencies</p>
+ *
+ * <p>Circular dependencies in fields and setter methods are absolutely okay.
+ * In constructors, they are absolutely <em>not</em> okay. JFIE will throw up
+ * if it notices even the possibility of any of that shit going on.</p>
  */
 public final class Jfie {
 
